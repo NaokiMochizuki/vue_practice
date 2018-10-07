fe27e02aa1b99b60ae0161c97e5437f0afbe995b
NaokiMochizuki <naoki.mochizuki1006@gmail.com>
2018-10-07 17:46:24 +0900

    2章完了

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
-// vueコンポーネントの作成
-// el: でDOM要素の指定
-// data: でコンポーネント内のデータを定義
-var app = new Vue({
-  el: '#app',
+var items = [
+  {
+    name: '鉛筆',
+    price: 300,
+    quantity: 0
+  },
+  {
+    name: 'ノート',
+    price: 400,
+    quantity: 0
+  },
+  {
+    name: '消しゴム',
+    price: 500,
+    quantity: 0
+  }
+]
+
+var vm = new Vue({
+  el: "#app",
   data: {
-    message: 'Hello Vue!'
+    items: items
+  },
+  filters: {
+    numberWithDelimiter: function(value){
+      if(!value){
+        return '0'
+      }
+      return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
+    }
+  },
+  methods: {
+    doBuy: function(){
+      alert(this.totalPriceWithTax + '円以上のお買い上げ')
+      this.items.forEach(function(item){
+        item.quantity = 0
+      })
+    }
+  },
+  computed: {
+    totalPrice: function(){
+      return this.items.reduce(function(sum, item){
+        return sum + (item.price * item.quantity)
+      }, 0)
+    },
+    totalPriceWithTax: function(){
+      return Math.floor(this.totalPrice * 1.08)
+    },
+    canBuy: function(){
+      return this.totalPrice >= 1000
+    },
+    errorMessageStyle: function(){
+      return{
+        border: this.canBuy ? '' : '1px solid red',
+        color: this.canBuy ? '' : 'red'
+      }
+    }
   }
 })
-
+window.vm = vm
